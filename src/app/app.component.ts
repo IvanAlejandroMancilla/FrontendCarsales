034123d3467c087b32e630968db5029ce8e5d69a
IVAN <IVAN.ALEJANDRO.MANCILLA@GMAIL.com>
2025-10-16 21:27:41 -0300

    agregando paginas y diseño simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomeComponent, NavbarComponent],
+    // RouterOutlet,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontendRickandMorty';
+  title = 'Prueba Tecnica';
 }
